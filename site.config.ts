a4bf27665c803fce34d15ecc2f81d7d7108d3cb2
lesoon-zwy <147805535+lesoon-zwy@users.noreply.github.com>
2023-10-13 16:48:56 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '3a42ccdb5f8e4f17a790363d862dacf1',
+  rootNotionPageId: '959e0884047940c58f8997dd8b0ad8ca?v=3a42ccdb5f8e4f17a790363d862dacf1',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: '丽迅素材库',
-  domain: 'https://www.notion.so/959e0884047940c58f8997dd8b0ad8ca?v=3a42ccdb5f8e4f17a790363d862dacf1',
+  domain: 'https://www.notion.so/lesoon/959e0884047940c58f8997dd8b0ad8ca?v=3a42ccdb5f8e4f17a790363d862dacf1',
   author: 'ZWY',
 
   // open graph metadata (optional)
