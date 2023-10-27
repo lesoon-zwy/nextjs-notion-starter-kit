cd4f5a04630b983273f6e79c44fe8d1f103a004f
lesoon-zwy <zhou.wy@lesoon.com>
2023-10-27 14:12:33 +0800

    Update SiteLogo.tsx

--- components/SiteLogo.tsx
+++ components/SiteLogo.tsx
@@ -3,7 +3,7 @@ import siteConfig from 'site.config';
 
 const Logo = () => {
   return (
-    <a href={siteConfig.domain}><img src={siteConfig.logoSrc} height="20rem" alt="Logo" /></a>
+    <a pageID={siteConfig.rootNotionPageId}><img src={siteConfig.logoSrc} height="20rem" alt="Logo" /></a>
   );
 }
 
