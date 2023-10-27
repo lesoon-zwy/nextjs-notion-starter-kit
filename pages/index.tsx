5aa961292da24416987af5f845c6dcaecf0a21f2
lesoon-zwy <zhou.wy@lesoon.com>
2023-10-27 16:19:26 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,36 @@
-import * as React from 'react';
-import { NotionPage } from '@/components/NotionPage';
-import { domain } from '@/lib/config';
-import { resolveNotionPage } from '@/lib/resolve-notion-page';
+import * as React from 'react'
+
+import { NotionPage } from '@/components/NotionPage'
+import { domain } from '@/lib/config'
+import { resolveNotionPage } from '@/lib/resolve-notion-page'
 
 export const getStaticProps = async () => {
   try {
-    const props = await resolveNotionPage(domain);
+    const props = await resolveNotionPage(domain)
 
-    return { props, revalidate: 10 };
+    return { props, revalidate: 10 }
   } catch (err) {
-    console.error('page error', domain, err);
-    throw err;
+    console.error('page error', domain, err)
+
+    // we don't want to publish the error version of this page, so
+    // let next.js know explicitly that incremental SSG failed
+    throw err
   }
-};
+}
 
 export default function NotionDomainPage(props) {
-  // Function to manipulate the content to hide the specified elements
-  const manipulateContent = (content) => {
-    const modifiedContent = content.map((child) => {
-      if (React.isValidElement(child) && child.type === 'a' && child.props.className === 'notion-page-link') {
-        // Hide <a> tags with class name 'notion-page-link'
-        return React.cloneElement(child, { style: { display: 'none' } });
-      }
-      return child;
-    });
-
-    return modifiedContent;
-  };
-
   return (
-    <div>
-      {/* Render NotionPage with manipulated content */}
-      <NotionPage {...props}>{manipulateContent(props.children)}</NotionPage>
+  <div>
+      <NotionPage {...props} />
+      {props.children}
       {/* Other content that should always be displayed */}
+      <style>
+        {`
+          .notion-page-link {
+            display: none;
+          }
+        `}
+      </style>
     </div>
   );
 }
