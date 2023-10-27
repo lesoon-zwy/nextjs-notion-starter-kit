88eeb7aa8d1869215d1612b145e4384be86143c1
lesoon-zwy <zhou.wy@lesoon.com>
2023-10-27 11:23:06 +0800

    delete breadcrumbs

--- components/NotionPageHeader.tsx
+++ components/NotionPageHeader.tsx
@@ -4,7 +4,7 @@ import * as types from 'notion-types'
 import { IoMoonSharp } from '@react-icons/all-files/io5/IoMoonSharp'
 import { IoSunnyOutline } from '@react-icons/all-files/io5/IoSunnyOutline'
 import cs from 'classnames'
-import { Breadcrumbs, Header, Search, useNotionContext } from 'react-notion-x'
+import { Header, Search, useNotionContext } from 'react-notion-x'
 
 import { isSearchEnabled, navigationLinks, navigationStyle } from '@/lib/config'
 import { useDarkMode } from '@/lib/use-dark-mode'
@@ -50,7 +50,7 @@ export const NotionPageHeader: React.FC<{
     <header className='notion-header'>
       <div className='notion-nav-header'>
         <Logo />
-        <Breadcrumbs block={block} rootOnly={false} />
+        
 
         <div className='notion-nav-header-rhs breadcrumbs'>
           {navigationLinks?.map((link, index) => {
